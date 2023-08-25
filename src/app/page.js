24b91cc7703923bdd4966c5b342e47a03056e23c
JvBelletati <jvbelletati@gmail.com>
2023-08-25 09:39:56 -0300

    Aula 25/08 - Cards

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
-import Titulo from '@/components/Titulo'
-import Image from 'next/image'
+import CardFilme from '@/components/CardFilme'
+import Titulo from '@/components/Titulo.jsx'
 
 export default function Home() {
+
+  //mock
+  const  filmes = [
+
+    {
+      titulo: "Star Wars",
+      nota:  9.5,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/uLlrDUtFG2tKtDcJN6kBznlqqsp.jpg"
+    },
+
+    {
+      titulo: "Barbie",
+      nota:  8.5,
+      poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+    }
+  ] 
+
+
   return ( //JSX
     <>
       <nav className="bg-slate-900 p-4">
@@ -9,6 +27,11 @@ export default function Home() {
       </nav>
 
       <Titulo>Em alta</Titulo>
+
+      <section className="flex flex-wrap gap-2">
+        {filmes.map( filme => <CardFilme filme={filme} /> )}
+      </section>
+
       <Titulo>Lançamentos</Titulo>
 
     </>
