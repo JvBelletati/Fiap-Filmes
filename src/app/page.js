787463774490e0a5dfdadd267a8cda3f53d09fef
JvBelletati <jvbelletati@gmail.com>
2023-09-15 09:43:39 -0300

    Aula 15/09 - API

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,23 @@
 import CardFilme from '@/components/CardFilme'
 import Titulo from '@/components/Titulo.jsx'
 
-export default function Home() {
+async function carregarDados() {
+  const url = "https://api.themoviedb.org/3/trending/movie/week?api_key=e29f83299859d229702074e8f70ce639&language=pt-br"
+  const response = await fetch(url)
+  const json = await response.json()
+  return json.results
+}
+
+export default async function Home() {
 
   //mock
-  const  filmes = [
-
-    {
-      id: 1,
-      titulo: "Star Wars",
-      nota:  9.5,
-      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/uLlrDUtFG2tKtDcJN6kBznlqqsp.jpg"
-    },
-
-    {
-      id: 2,
-      titulo: "Barbie",
-      nota:  8.5,
-      poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
-    }
-  ] 
-
+  const  filmes = await carregarDados()  
 
   return ( //JSX
     <>
-      <nav className="bg-slate-900 p-4">
+      <nav className="flex items-end gap-4 bg-slate-900 p-4">
         <h1 className="text-3xl font-bold">Fiap Filmes</h1>
+        <a href="/favoritos">Favoritos</a>
       </nav>
 
       <Titulo>Em alta</Titulo>
